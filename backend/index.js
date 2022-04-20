5a3806e12067d02d4870c810f8522d28985c8707
BurnRadish <56288133+BurnRadish@users.noreply.github.com>
2022-04-20 15:01:19 +0700

    Update schema

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,28 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
- 
- 
-
- 
+const mongooose = require('mongoose')
 const schema = require('./schema/schema')
- 
 
- 
- 
+
+
 const app = express();
- 
- 
- 
-//This route will be used as an endpoint to interact with Graphql,
- 
-//All queries will go through this route.
- 
+
+
+//set up mongoDB
+mongooose.connect('mongodb+srv://admin:reverse121254@cluster0.rrnkc.mongodb.net/test');
+
+mongooose.connection.once('open', () => {
+   console.log('connected to MongoDB');
+});
+
 app.use('/graphql', graphqlHTTP({
- 
-   //directing express-graphql to use this schema to map out the graph
- 
    schema,
- 
-   //directing express-graphql to use graphiql when goto '/graphql' address in the browser
- 
-   //which provides an interface to make GraphQl queries
- 
    graphiql:true
- 
 }));
  
 
  
  
-app.listen(3000, () => {
- 
+app.listen(process.env.PORT || 3000, () => {
    console.log('Listening on port 3000');
- 
 });
