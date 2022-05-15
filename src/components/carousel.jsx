ba3ab49caca79b98fbccf913163cbb70aa44cfdf
it62070180 <56427172+it62070180@users.noreply.github.com>
2022-05-15 16:24:07 +0700

    fix : Craousel Url
    Up ความละเอียดรูปใหม่

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -7,16 +7,16 @@ function Carousel() {
         <Container>
             <CCarousel controls indicators>
               <CCarouselItem>
-                <CImage className="d-block w-100" src="https://i.imgur.com/GLBz7Qg.jpg" alt="slide 1" width={"100%"} height={"100%"} />
+                <CImage className="d-block w-100" src="https://i.imgur.com/pkIMEpB.jpg" alt="slide 1" width={"100%"} height={"100%"} />
               </CCarouselItem>
               <CCarouselItem>
-                <CImage className="d-block w-100" src="https://i.imgur.com/r9VjHHN.jpg" alt="slide 2" width={"100%"} height={"100%"} />
+                <CImage className="d-block w-100" src="https://i.imgur.com/gWdDewn.jpg" alt="slide 2" width={"100%"} height={"100%"} />
               </CCarouselItem>
               <CCarouselItem>
-                <CImage className="d-block w-100" src="https://i.imgur.com/7HQFwDb.jpg" alt="slide 3" width={"100%"} height={"100%"} />
+                <CImage className="d-block w-100" src="https://i.imgur.com/PuemOmQ.jpg" alt="slide 3" width={"100%"} height={"100%"} />
               </CCarouselItem>
               <CCarouselItem>
-                <CImage className="d-block w-100" src="https://i.imgur.com/qm18qGh.jpg" alt="slide 4" width={"100%"} height={"100%"} />
+                <CImage className="d-block w-100" src="https://i.imgur.com/78Cvwgw.jpg" alt="slide 4" width={"100%"} height={"100%"} />
               </CCarouselItem>
             </CCarousel>
         </Container>
