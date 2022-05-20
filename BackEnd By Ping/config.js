84a4c1fcaa578ee01dfabfb7c6f057cb37f01f81
Supakorndev <56401869+Supakorndev@users.noreply.github.com>
2022-05-20 15:00:27 +0700

    Delete config.js

--- BackEnd By Ping/config.js
+++
@@ -1,5 +0,0 @@
-const { MongoClient } = require('mongodb')
-const uri = "mongodb+srv://valpo:MbV80QsSvDl7Cypi@cluster0.n06qh.mongodb.net";
-const client = new MongoClient(uri);
-
-module.exports = client;
