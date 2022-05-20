e34426c9290c7163193a900bf0e7fc74db7688c3
Sirapop Mansamk <56288133+BurnRadish@users.noreply.github.com>
2022-05-20 17:55:01 +0700

    Restore package and page

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -5,6 +5,7 @@ import ShowLoginpage from "./pages/loginpage";
 import ShowHomepage from "./pages/homepage";
 import ShowBookpage from "./pages/bookpage";
 import ShowAccountpage from "./pages/accountpage";
+import ShowRegisterpage from "./pages/registerpage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       {/* <Container> */}
         <Routes>
           <Route path="/" element={<ShowLoginpage/>} />
+          <Route path="/register" element={<ShowRegisterpage/>} />
           <Route path="/homepage" element={<ShowHomepage/>} />
           <Route path="/bookpage" element={<ShowBookpage/>} />
           <Route path="/accountpage" element={<ShowAccountpage/>} />
