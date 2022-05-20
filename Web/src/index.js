3b13506baf7e4d6eab15f9ba987c2aad2d0a947c
Sirapop Mansamk <56288133+BurnRadish@users.noreply.github.com>
2022-05-20 18:00:17 +0700

    fix router
    - create root for react ver.18
    - fix hash router to browser router

--- Web/src/index.js
+++ Web/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
-import { HashRouter  } from "react-router-dom";
+import {
+  BrowserRouter,
+} from "react-router-dom";
 import '@coreui/coreui/dist/css/coreui.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+
+root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
 );
