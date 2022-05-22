bf7bcccc3c24d25645a80595b52492af800f9dfa
it62070180 <56427172+it62070180@users.noreply.github.com>
2022-05-23 01:09:10 +0700

    Update : mui icon
    Use material-ui Icon

--- Web/src/components/detailcard.jsx
+++ Web/src/components/detailcard.jsx
@@ -1,21 +1,37 @@
 import Container from '@mui/material/Container';
 import { Grid } from '@mui/material';
-
+import NoFoodIcon from '@mui/icons-material/NoFood';
+import SmokeFreeIcon from '@mui/icons-material/SmokeFree';
+import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
+import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
 import * as React from 'react';
 const DetailsCard = () => {
     return(
         <Grid container>
             <Container>
                     <Grid item xs={12}>
-                        <h1>กฎการใช้งานโรงยิมสนามแบต</h1>
+                        <h1>กฎการใช้งานโรงยิมสนามแบดมินตัน</h1>
                     </Grid>
                     <hr style={{width:"50%",height:"2%", marginLeft:0}}></hr>
                     <Grid item xs={6}>
-                        <ul>
-                          <li>ไม่สูบบุหรี่ภายในพื่นที่โรงยิม</li>
-                          <li>เล่นกีฬากันด้วยฉันมิตรไม่ทะเลาะวิวาท</li>
-                          <li>ไม่เอาของกินเข้ามากินในโรงยิม</li>
-                          <li>รักษาความสะอาดด้วย</li>
+                        <ul style={{listStyleType:'none',fontSize:25}}>
+                          <li >
+                            <SmokeFreeIcon fontSize='large' className='me-3'/>
+                              ไม่สูบบุหรี่ภายในพื่นที่โรงยิม
+                              </li>
+                          <li>
+                            <SentimentVerySatisfiedIcon fontSize='large' className='me-3'/> 
+                            
+                              เล่นกีฬากันด้วยฉันมิตรไม่ทะเลาะวิวาท
+                              </li>
+                          <li>
+                              <NoFoodIcon fontSize='large' className='me-3'/>
+                              ไม่เอาของกินเข้ามากินในโรงยิม
+                              </li>
+                          <li>
+                              <CleaningServicesIcon fontSize='large' className='me-3'/>
+                              รักษาความสะอาดด้วย
+                              </li>
                         </ul>
                     </Grid>             
             </Container>
