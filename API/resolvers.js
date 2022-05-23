9bba06786c0c91d71094cc6e229c4a13bb1907f8
Supakorndev <56401869+Supakorndev@users.noreply.github.com>
2022-05-23 19:32:36 +0700

    Add files via upload

---
+++ API/resolvers.js
@@ -0,0 +1,76 @@
+const Users = require("./models/Fetch")
+const Regis = require("./models/register")
+const findUser = require("./models/queryUser")
+const Addstate = require("./models/addstate")
+const client = require("./config");
+const {generateToken} = require("./utils/genToken")
+const resolvers = {
+    Query:{
+        courts: async()=>{
+            const user = await Users.find();
+            return user;
+        },
+    },
+    Mutation:{
+        addUsers: async(parent, args, ctx, info) =>{
+            const {id, name, state} = args
+            const user = new Users({
+                id: id,
+                name: name,
+                state: state
+            })
+            await user.save()
+            return user;
+        },
+        editUsers: async(parent, args, ctx, info) => {
+            const {id, name, state} = args
+            const user = await Users.findOneAndUpdate({id:id}, {name:name, state:state}, {new: true})
+            return user;
+        },
+        delete: async(parent, args, ctx, info)=>{
+            const {id, name, state} = args
+            const user = await Users.findOneAndRemove({id:id})
+            return user;
+        },
+        register: async(parent, args, ctx, info)=>{
+            const {email, password, name, surname, stu_id} = args
+            const regis = new Regis({
+                email: email,
+                password: password,
+                name: name,
+                surname: surname,
+                stu_id: stu_id,
+                role: "user",
+                token: generateToken()
+            })
+            await regis.save()
+            return regis;
+        },
+        login: async(parent, args)=>{
+            const {email, password} = args
+            const login = await Regis.findOne({email:email, password:password});
+            return login;
+        },
+        addstate: async(parent, args, ctx, info)=>{
+            const {id, detail, status, index} = args
+            const add = await Users.findOneAndUpdate({id:id}, {$push:{state:{detail, status,index}}}, {new:true})
+            return add;
+        },
+        editstate: async(parent, args, ctx, info)=>{
+            const {id, index, detail, status} = args
+            const edit = await Users.updateOne({id:id, 'state.index': index}, {$set:{
+                "state.$.detail":detail,
+                "state.$.status":status,
+                "state.$.index":index
+            }})
+            return edit;
+        },
+        queryUser: async(parent, args, ctx, info)=>{
+            const {stu_id} = args
+            const user = await findUser.findOne({stu_id:stu_id});
+            return user;
+        }
+    }
+}
+
+module.exports = resolvers
---
+++ API/typeDefs.js
@@ -0,0 +1,74 @@
+const typeDefs = `
+    type Query {
+        courts: [FullData!],
+        id: Int!,
+        index: Int!
+    }
+
+    type State {
+        detail: Detail,
+        status: Int!,
+        index: Int!
+    }
+
+    type Register {
+        email: String!,
+        password: String!,
+        name : String!,
+        surname: String!,
+        stu_id: String!,
+        role: String!,
+        token: String! 
+    }
+
+    type Login {
+        email: String!,
+        password: String!,
+    }
+
+    type Detail {
+        stuInfo1: String,
+        stuInfo2: String,
+        stuInfo3: String,
+        stuInfo4: String,
+    }
+
+    type FullData {
+        id: Int!
+        name: String!
+        state: [State]
+    }
+
+    type Mutation {
+        addUsers(id: Int!, name: String!, state: [Input]): [FullData],
+        editUsers(id: Int!, name: String!, state: [Input]): [FullData],
+        delete(id: Int!, name: String!, state:[Input]): [FullData],
+        register(email: String!, password: String!, name: String!, surname: String!, stu_id: String!): Register,
+        login(email: String!, password: String!): Register,
+        addstate(id: Int!,detail: court, status: Int!, index: Int!): [FullData],
+        editstate(id: Int!, index: Int!, detail: court, status: Int!): [FullData],
+        queryUser(stu_id: String!): Register
+    }
+
+    input court {
+        stuInfo1: String,
+        stuInfo2: String,
+        stuInfo3: String,
+        stuInfo4: String,
+    }
+
+    input Input {
+        detail : Super!,
+        status : Int!,
+        index: Int!
+    }
+
+    input Super {
+        stuInfo1 : String!,
+        stuInfo2 : String!,
+        stuInfo3 : String!,
+        stuInfo4 : String!,
+    }
+`
+
+module.exports = typeDefs;
