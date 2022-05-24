2435363ab4bcc2477e5bfd6c34164410f2fb76e1
supakorn <supakorn.ping.k@gmail.com>
2022-05-24 22:48:14 +0700

    update

--- Web/src/components/time.jsx
+++ Web/src/components/time.jsx
@@ -1,30 +1,74 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import gql from 'graphql-tag'
+import {print} from 'graphql'
 
 const ShowTime = () => {
-    const [today, setToday] = useState(new Date());
-    const [date, setDate] = useState("")
-    const [time, setTime] = useState("")
-    useEffect(
-        () => {
-            const interval = setInterval(() => {
-                setToday(new Date())
-                setDate(today.getDate().toString().padStart(2, "0")+'/'+(today.getMonth()+1).toString().padStart(2, "0")+'/'+today.getFullYear())
-                setTime(today.getHours().toString().padStart(2, "0") + ":" + today.getMinutes().toString().padStart(2, "0") + ":"+today.getSeconds().toString().padStart(2, "0"))
-            }, 100)
-            return () => clearInterval(interval);
-        },
-        [today]
-    )
-    return (
-        <>
-            <div style={{textAlign: "center"}}>
-                <h3 style={{marginBottom: 0}}>{date}</h3>
-                <h1 style={{marginTop: 0}}>{time}</h1>
-            </div>
+  const [today, setToday] = useState(new Date());
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const resetallstate = gql`
+    mutation resetallstate(
+      $detail: court
+    ) {
+      resetallstate(
+        detail: $detail
+      ) {
+        state {
+          detail {
+            stuInfo1
+            stuInfo2
+            stuInfo3
+            stuInfo4
+          }
+        }
+      }
+    }
+  `;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setToday(new Date());
+      setDate(
+        today.getDate().toString().padStart(2, "0") +
+          "/" +
+          (today.getMonth() + 1).toString().padStart(2, "0") +
+          "/" +
+          today.getFullYear()
+      );
+      setTime(
+        today.getHours().toString().padStart(2, "0") +
+          ":" +
+          today.getMinutes().toString().padStart(2, "0") +
+          ":" +
+          today.getSeconds().toString().padStart(2, "0")
+      );
+    }, 100);
+    return () => clearInterval(interval);
+  }, [today]);
+  if (time == "00:00:00") {
+    axios.post("http://localhost:4000/graphql", {
+        query: print(resetallstate),
+        variables:{
+            detail: {
+                stuInfo1: "",
+                stuInfo2: "",
+                stuInfo3: "",
+                stuInfo4: ""
+    
+            }
+        }
+    }).then((res)=>{
+        console.log("clear all state")
+    });
+  }
+  return (
+    <>
+      <div style={{ textAlign: "center" }}>
+        <h3 style={{ marginBottom: 0 }}>{date}</h3>
+        <h1 style={{ marginTop: 0 }}>{time}</h1>
+      </div>
+    </>
+  );
+};
 
-        </>
-    )
-
-}
-
-export default ShowTime
+export default ShowTime;
--- Web/src/pages/accountpage.jsx
+++ Web/src/pages/accountpage.jsx
@@ -1,6 +1,22 @@
 import { Form, Button } from 'react-bootstrap';
+import axios from "axios"
+import { createContext, useEffect, useState } from "react"
 
 const ShowAccountpage = () => {
+    const [name, setname] = useState("");
+    const [surname, setsurname] = useState("")
+    const [id, setid] = useState("")
+    const [email, setemail] = useState("")
+    useEffect(()=>{
+        if(!localStorage.getItem("Token") == false){
+            setname(localStorage.getItem("Name"))
+            setsurname(localStorage.getItem("Surname"))
+            setid(localStorage.getItem("ID"))
+            setemail(localStorage.getItem("ID")+"@kmitl.ac.th")
+        }else{
+            alert("Please Login First")
+        }
+    },[])
     return (
         <Form className="container-fluid">
         <div className='container'  style={{width:'50%'}}>
@@ -9,7 +25,7 @@ const ShowAccountpage = () => {
             <div className="col-6">
                     <Form.Group className="mb-1" controlId="formBasicName">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="Name" placeholder={localStorage.getItem('Name')}/>
+                        <Form.Control type="Name" placeholder={localStorage.getItem('Name')} value={name}/>
                         <Form.Text className="text-muted">
                          This is your name
                         </Form.Text>
@@ -18,7 +34,7 @@ const ShowAccountpage = () => {
                 <div className="col-6">
                 <Form.Group className="mb-1" controlId="formBasicSurname">
                         <Form.Label>Surname</Form.Label>
-                        <Form.Control type="Name" placeholder={localStorage.getItem('Surname')}/>
+                        <Form.Control type="Name" placeholder={localStorage.getItem('Surname')} value={surname}/>
                         <Form.Text className="text-muted">
                          This is your Surname
                         </Form.Text>
@@ -27,7 +43,7 @@ const ShowAccountpage = () => {
                 <div className="col-12">
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Your email"/>
+                        <Form.Control type="email" placeholder="Your email" value={email}/>
                         <Form.Text className="text-muted">
                           We'll never share your email with anyone else.
                         </Form.Text>
@@ -37,7 +53,7 @@ const ShowAccountpage = () => {
                 <div className="col-6">
                 <Form.Group className="mb-1" controlId="formBasicStudent Id">
                         <Form.Label>Student Id</Form.Label>
-                        <Form.Control type="Name" placeholder="Student Id"/>
+                        <Form.Control type="Name" placeholder="Student Id" value={id}/>
                         <Form.Text className="text-muted">
                          This is your Student id
                         </Form.Text>
