192a5715cc79a41d858cfdb7339eaec0488928b0
Arpo201 <arpo.s201@gmail.com>
2023-01-30 13:15:02 +0700

    Update readme

--- Web/src/variable.js
+++ Web/src/variable.js
@@ -1,1 +1,2 @@
-export const API_URL = "https://badminton-court.herokuapp.com/graphql"
+// export const API_URL = "https://badminton-court.herokuapp.com/graphql"
+export const API_URL = "http://localhost:4000/graphql"
